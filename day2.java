9b53e16a466048d1a1573eab9a9845c4242eaa64
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-09 19:19:11 +0800

    printf

---
+++ day2.java
@@ -0,0 +1,9 @@
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("Days 2");
+        System.out.print("Nama Saya : ");
+        System.out.print("Achmad Aidhil");
+        double nilai = 88.2234;
+        System.out.printf("%nNilai : %.2f", nilai);
+    }
+}
