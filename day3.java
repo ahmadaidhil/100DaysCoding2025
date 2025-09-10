16cb4b7ec8de24ae4c7739a09c4f9eab5b33d7f5
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-10 11:35:34 +0800

    Memahami dan menggunakan Komentar

---
+++ day3.java
@@ -0,0 +1,10 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("latihan Komentar");
+        // System.out.println("Ini komen satu baris");
+
+        /*System.out.println("Ini komen banyak baris");
+        System.out.println("Ini komen banyak baris");
+        System.out.println("Ini komen banyak baris");*/
+    }
+}
