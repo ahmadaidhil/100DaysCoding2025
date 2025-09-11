42010fea9c890495fa15efe54b433468087aecc1
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-11 19:47:32 +0800

    Escape Sequence (\n, \t, dan lain lain).

---
+++ day4.java
@@ -0,0 +1,15 @@
+public class day4 {
+    public static void main(String[] args) {
+        // Baris baru
+        System.out.println("Halo\nDunia");
+
+        // Tab
+        System.out.println("Nama:\tAchmad Aidhil");
+
+        // Tanda kutip ganda di dalam string
+        System.out.println("Dia berkata: \"Belajar Java itu seru!\"");
+
+        // Backslash
+        System.out.println("Folder ada di C:\\Users\\Achmad");
+    }
+}
