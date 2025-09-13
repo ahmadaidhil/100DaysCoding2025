0eda7d420d21814ce3628940e459573d943646fe
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-13 20:25:22 +0800

    Tipe Data Pecahan (double, float).

---
+++ day6.java
@@ -0,0 +1,9 @@
+public class day6 {
+    public static void main(String[] args) {
+        float tinggiBadan = 162.5f;
+        double beratBadan = 52.3;
+
+        System.out.println("Tinggi badan: " + tinggiBadan);
+        System.out.println("Berat badan: " + beratBadan);
+    }
+}
