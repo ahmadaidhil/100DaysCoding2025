0f3f1be73bf616c0432fe1a611820b8653613e4e
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-14 15:55:18 +0800

    Tipe Data Karakter (char) dan Boolean (boolean).

---
+++ day7.java
@@ -0,0 +1,8 @@
+public class day7 {
+    public static void main(String[] args) {
+        char predikat = 'A';
+        System.out.println("Predikat DDP : " + predikat);
+        boolean status = true;
+        System.out.println("Lulus(true/false) : " + status);
+    }
+}
