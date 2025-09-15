7643048227da6485b03379377566a7d19c456bfb
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-15 20:27:15 +0800

    Pengenalan Tipe Data String untuk teks.

---
+++ day8.java
@@ -0,0 +1,9 @@
+public class day8 {
+    public static void main(String[] args) {
+        String nama = "Achmad Aidhil";
+        String nim = "D0224308";
+        String status = "Mahasiswa";
+
+        System.out.println("Nama\t: " + nama + "\nNIM\t: " + nim + "\nStatus\t: " + status);
+    }
+}
