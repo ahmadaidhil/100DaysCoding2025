f597bf4900420eb9fe5f4b6de7fac97293543a45
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-16 09:56:07 +0800

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ day009.java
@@ -0,0 +1,9 @@
+public class day009 {
+    public static void main(String[] args) {
+        int angka;  // deklarasi variabel
+        angka = 10; // inisialisasi variabel
+        System.out.println("angka : " + angka);
+        angka = 20; // update variabel
+        System.out.println("angka terupdate : " + angka);
+    }
+}
