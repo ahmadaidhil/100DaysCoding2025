df31b727b62022f2e045a6a1e77badf623bc0409
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-17 21:02:53 +0800

    Constanta (Final Variabel).

---
+++ day010.java
@@ -0,0 +1,8 @@
+public class day010 {
+    public static void main(String[] args) {
+        final int angka = 2025; // final variable adalah variable tetap/tidak bisa diubah
+        System.out.println("Final variabel: " + angka);
+        // angka = 2026; // tidak bisa di ubah
+
+    }
+}
