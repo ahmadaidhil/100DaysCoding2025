c0d4e0c6d6af4791b13db0dc5e972e675e22c559
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-21 10:57:46 +0800

    Mengubah String menjadi Tipe Data Primitif

---
+++ day014.java
@@ -0,0 +1,8 @@
+public class day014 {
+    public static void main(String[] args) {
+        String angkaString = "12345";
+        int angkaInt = Integer.parseInt(angkaString);
+        System.out.println("Angka dalam bentuk String: " + angkaString);
+        System.out.println("Angka dalam bentuk Integer: " + angkaInt);
+    }
+}
