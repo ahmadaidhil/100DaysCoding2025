059a6b46ce625a5b048de8b1b2b136174f02ac5b
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-23 21:11:02 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ day016.java
@@ -0,0 +1,14 @@
+public class day016 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Perkalian
+        int kali = a * b;
+        System.out.println("Hasil Perkalian: " + kali);
+        // Pembagian
+        int bagi = b / a;
+        System.out.println("Hasil Pembagian: " + bagi);
+    }
+
+}
