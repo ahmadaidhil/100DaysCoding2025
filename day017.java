f70ecfe8fb5a73c65c0ab365a357e2fd204959a0
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-24 15:06:01 +0800

    Operator Aritmatika Modulus atau Sisa Bagi (%).

---
+++ day017.java
@@ -0,0 +1,9 @@
+public class day017 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 22;
+        // Modulus
+        int mod = b % a;
+        System.out.println("Hasil Modulus: " + mod);
+    }
+}
