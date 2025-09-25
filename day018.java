0c11cdbd5b3caa97facdac01f248eb4d50ae738a
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-25 21:41:09 +0800

    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ day018.java
@@ -0,0 +1,17 @@
+public class day018 {
+    public static void main(String[] args) {
+        byte a = 120;
+        short b = a;
+        int c = b;
+        long d = c;
+        float e = d;
+        double f = e;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
