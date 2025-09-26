8a253f768aa2c7f328fcc1da62b506ff1c6782c3
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-26 21:55:47 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ day019.java
@@ -0,0 +1,8 @@
+public class day019 {
+    public static void main(String[] args) {
+        double a = 12.56;
+        int b = (int) a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
