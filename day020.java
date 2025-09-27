9ab386c5fb3cc274c272e6551cecf57541e65bc9
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-27 21:56:52 +0800

    Mengubah Tipe Data Primitif menjadi String.

---
+++ day020.java
@@ -0,0 +1,8 @@
+public class day020 {
+    public static void main(String[] args) {
+        int a = 12345;
+        String b = Integer.toString(a);
+        System.out.println(a + 100);
+        System.out.println(b + 100);
+    }
+}
