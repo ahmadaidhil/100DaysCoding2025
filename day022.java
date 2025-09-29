ec0e39620539698dc4bb097e6fc8166c5a99dfe4
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-09-29 14:56:17 +0800

    Latihan: Program Luas Persegi.

---
+++ day022.java
@@ -0,0 +1,8 @@
+public class day022 {
+    public static void main(String[] args) {
+        int sisi = 10;
+        int luas = sisi * sisi;
+        System.out.println("Sisi: " + sisi);
+        System.out.println("Luas Persegi: " + luas);
+    }
+}
