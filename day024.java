4f63f019af96458964bad01f0a2ad1794512780d
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-01 09:28:04 +0800

    Latihan: Program Luas Lingkaran.

---
+++ day024.java
@@ -0,0 +1,10 @@
+public class day024 {
+    public static void main(String[] args) {
+        final double phi = 3.14;
+
+        double r = 7.0;
+
+        double luas = phi * r * r;
+        System.out.println("Luas lingkaran dengan jari-jari " + r + " adalah: " + luas);
+    }
+}
