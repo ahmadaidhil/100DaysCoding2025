89f48385b5f71f9eb28ff3c4cba890ae138b9690
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-02 09:26:27 +0800

    Operator Increment dan Decrement (++, --).

---
+++ day025.java
@@ -0,0 +1,14 @@
+public class day025 {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 20;
+        angka1++;
+        System.out.println("Angka1: " + angka1);
+        angka1++;
+        System.out.println("Angka1: " + angka1);
+        angka2--;
+        System.out.println("Angka2: " + angka2);
+        angka2--;
+        System.out.println("Angka2: " + angka2);
+    }
+}
