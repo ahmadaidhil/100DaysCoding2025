197a2fa8bd4797472263afc874a9b6c69a0e1bf3
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-03 21:38:06 +0800

    Operator Penugasan (=, +=, -=, dll).

---
+++ day026.java
@@ -0,0 +1,16 @@
+public class day026 {
+    public static void main(String[] args) {
+        int angka = 10;
+
+        angka += 2;
+        System.out.println("angka: " + angka);
+        angka -= 2;
+        System.out.println("angka: " + angka);
+        angka *= 5;
+        System.out.println("angka: " + angka);
+        angka /= 4;
+        System.out.println("angka: " + angka);
+        angka %= 4;
+        System.out.println("angka: " + angka);
+    }
+}
