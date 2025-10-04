547ce9bccd74f9f4faf0af31733d13f73da18702
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-04 12:18:52 +0800

    Evaluasi.

--- day027.java
+++ day027.java
@@ -4,6 +4,7 @@ public class day027 {
         Scanner input = new Scanner(System.in);
         int p = input.nextInt();
         int l = input.nextInt();
+        
         System.out.println((p * l) / 4);
     }
 }
