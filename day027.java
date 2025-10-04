c4bf79f6f433357b9317902f6a7532a082988b35
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-04 12:17:48 +0800

    Create day027.java

---
+++ day027.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class day027 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p = input.nextInt();
+        int l = input.nextInt();
+        System.out.println((p * l) / 4);
+    }
+}
