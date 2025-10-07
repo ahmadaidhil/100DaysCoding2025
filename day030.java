f50db3e3b4fdd58a79bee6f3736d67f5a716cb66
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-07 11:13:48 +0800

    Operator Perbandingan <= dan >=.

---
+++ day030.java
@@ -0,0 +1,11 @@
+public class day030 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 2;
+        System.out.println(a >= b);
+        System.out.println(a >= c);
+        System.out.println(a <= b);
+        System.out.println(a <= c);
+    }
+}
