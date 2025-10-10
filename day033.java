2b2cf7591b1fce1e2bda457042123651132c16ae
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-10 20:53:28 +0800

    Operator Logika NOT (!).

---
+++ day033.java
@@ -0,0 +1,9 @@
+public class day033 {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println("!benar = " + !benar);
+        System.out.println("!salah = " + !salah);
+    }
+}
