001a67e2b6dfa6de9d9f15e5ac8f507eba5456b3
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-11 23:42:55 +0800

    Latihan: Mengkombinasikan berbagai operator.

--- day034.java
+++ day034.java
@@ -4,5 +4,16 @@ public class day034 {
         int tahun = 2024;
         boolean kabisat = (tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0);
         System.out.println(tahun + " adalah tahun kabisat: " + kabisat);
+
+        System.out.println("================================");
+        int a = 10;
+        int b = 20;
+        int c = 15;
+        int operasi1 = a + b * c;
+        int operasi2 = a * b + c;
+        System.out.println("Operasi 1 (a + b * c) = " + operasi1);
+        System.out.println("Operasi 2 (a * b + c) = " + operasi2);
+        System.out.println("Operasi 1 lebih besar = " + (operasi1 > operasi2) + ", habis dibagi 2 = " + (operasi1 % 2 == 0));
+        System.out.println("Operasi 2 lebih besar = " + (operasi1 < operasi2) + ", habis dibagi 2 = " + (operasi2 % 2 == 0));
     }
 }
