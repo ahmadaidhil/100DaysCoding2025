819dd07b6355627d47456ce46382c58dfc3a3624
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-12 14:50:55 +0800

    Percabangan (if).

---
+++ day035.java
@@ -0,0 +1,11 @@
+public class day035 {
+    public static void main(String[] args) {
+        boolean nilai = true;
+
+        if (nilai == true) {
+            System.out.println("Nilai benar");
+        } else {
+            System.out.println("Nilai salah");
+        }
+    }
+}
