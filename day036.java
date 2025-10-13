f48131315dd1d35d46842638d14024755604641d
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-13 20:41:44 +0800

    Nested if.

--- day036.java
+++ day036.java
@@ -1,44 +1,37 @@
 import java.util.Scanner;
-public class BersarangMenuMakanan {
+public class day036 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double pilihan, porsi=0, harga=0, jumlah, diskon=0;
-        String menu = "";
-        
-        System.out.println("## Menu Makanan ##");
-        System.out.println("1. Bakso Lokal\n2. Bakso Luar Negeri");
-        System.out.print("Pilih Menu: ");
-        pilihan = input.nextDouble();
-        
-        if (pilihan == 1) {
-            System.out.print("Berapa Porsi : ");
-            porsi = input.nextDouble();
-            menu = "Bakso Lokal";
-            harga = 10000;
-        } else if (pilihan == 2) {
-            System.out.print("Berapa Porsi : ");
-            porsi = input.nextDouble();
-            menu = "Bakso Luar Negeri";
-            harga = 12000;
-            
-            if (porsi >= 10) {
-                System.out.println("Dapat Diskon 10%");
-                diskon = 10 *(harga *  porsi) / 100;
+        String status = "";
+        System.out.print("Masukkan total belanjaan: ");
+        int total = input.nextInt();
+        System.out.print("Punya Member (Y/N)? : ");
+        char member = input.next().charAt(0);
+        double diskon = 0;
+        if (member == 'Y' || member == 'y') {
+            if (total >= 500000) {
+                diskon = 0.2;
             } else {
-                System.out.println("Diskon Minimal 10 Porsi");
+                diskon = 0.05;
             }
+            status = "ya";
+        } else if (member == 'N' || member == 'n') {
+            if (total > 500000) {
+                diskon = 0.1;
+            } else {
+                diskon = 0;
+            }
+            status = "tidak";
         } else {
-            System.err.println("Pilihan tidak valid");
-            System.exit(0);
+            System.out.println("Input tidak valid");
         }
-        
-        System.out.println("======== Pesanan Anda =========");
-        System.out.println("Menu : " + menu);
-        int value = (int)porsi;
-        System.out.println("Porsi : " + value);
-        jumlah = (harga * porsi) - diskon;
-        int value2 = (int)jumlah;
-        System.out.println("Total Pesanan = Rp. " + value2);
+
+        System.out.println("=== Rincian Belanjaan ===");
+        System.out.println("Total Belanjaan: " + total);
+        System.out.println("Status Member:" + status);
+        System.out.println("Diskon: " + (diskon * 100) + "%");
+        System.out.println("Total Bayar: Rp" + (total - (total * diskon)));
+
     }
     
 }
