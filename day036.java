14ba474062c1ca8a21d0ce431715779fe80bd07b
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-13 20:09:21 +0800

    Nested if.

--- day036.java
+++ day036.java
@@ -1,19 +1,44 @@
 import java.util.Scanner;
-public class day036 {
+public class BersarangMenuMakanan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int nilai = input.nextInt();
-
-        if (nilai >= 90) {
-            System.out.println("Nilai A");
-        } else if (nilai >= 80) {
-            System.out.println("Nilai B");
-        } else if (nilai >= 70) {
-            System.out.println("Nilai C");
-        } else if (nilai >= 60) {
-            System.out.println("Nilai D");
+        double pilihan, porsi=0, harga=0, jumlah, diskon=0;
+        String menu = "";
+        
+        System.out.println("## Menu Makanan ##");
+        System.out.println("1. Bakso Lokal\n2. Bakso Luar Negeri");
+        System.out.print("Pilih Menu: ");
+        pilihan = input.nextDouble();
+        
+        if (pilihan == 1) {
+            System.out.print("Berapa Porsi : ");
+            porsi = input.nextDouble();
+            menu = "Bakso Lokal";
+            harga = 10000;
+        } else if (pilihan == 2) {
+            System.out.print("Berapa Porsi : ");
+            porsi = input.nextDouble();
+            menu = "Bakso Luar Negeri";
+            harga = 12000;
+            
+            if (porsi >= 10) {
+                System.out.println("Dapat Diskon 10%");
+                diskon = 10 *(harga *  porsi) / 100;
+            } else {
+                System.out.println("Diskon Minimal 10 Porsi");
+            }
         } else {
-            System.out.println("Nilai E");
+            System.err.println("Pilihan tidak valid");
+            System.exit(0);
         }
+        
+        System.out.println("======== Pesanan Anda =========");
+        System.out.println("Menu : " + menu);
+        int value = (int)porsi;
+        System.out.println("Porsi : " + value);
+        jumlah = (harga * porsi) - diskon;
+        int value2 = (int)jumlah;
+        System.out.println("Total Pesanan = Rp. " + value2);
     }
+    
 }
