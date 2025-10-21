7029a6a3a57650e38e34b9855e55e08c3c50a0b7
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-10-21 22:14:43 +0800

    Latihan: Program Predikat Nilai (A,B,C,D,E).

--- day044.java
+++ day044.java
@@ -5,13 +5,13 @@ public class day044 {
         System.out.print("Masukkan nilai : ");
         int angka = input.nextInt();
         
-        if (angka >= 90) {
+        if (angka >= 90 && angka <= 100) {
             System.out.println("Nilai A");
-        } else if (angka >= 80) {
+        } else if (angka >= 80 && angka < 90) {
             System.out.println("Nilai B");
-        } else if (angka >= 70) {
+        } else if (angka >= 70 && angka < 80) {
             System.out.println("Nilai C");
-        } else if (angka >= 60) {
+        } else if (angka >= 60 && angka < 70) {
             System.out.println("Nilai D");
         } else {
             System.out.println("Nilai E");
