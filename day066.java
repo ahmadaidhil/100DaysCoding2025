5c9dd97d15611eda9028b052ec577eab6e03d73d
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-11-12 21:35:11 +0800

    Latihan: Membuat Pola Vertikal.

---
+++ day066.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class day066 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka : ");
+        int angka = input.nextInt();
+
+        for (int i = 1; i <= angka; i++) {
+            System.out.println("-");
+        }
+    }
+}
