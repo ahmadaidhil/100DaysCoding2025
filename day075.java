4e2d2176318b74bcc2cb333129aecf5000b9a642
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-11-21 22:14:05 +0800

    Method String Dasar (length(), toUpperCase(), toLowerCase()).

---
+++ day075.java
@@ -0,0 +1,10 @@
+public class day075 {
+    public static void main(String[] args) {
+        
+        String nama = "Achmad Aidhil";
+
+        System.out.println("Length     : " + nama.length());
+        System.out.println("Upper Case : " + nama.toUpperCase());
+        System.out.println("Lower Case : " + nama.toLowerCase());
+    }
+}
