898c4421a3a41c61d959202e917767f53432e1ac
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-11-26 23:23:34 +0800

    Array 1D.

---
+++ day080.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class day080 {
+    public static void main(String[] args) {
+        String[] nama = {"Andi", "Budi", "Caca", "Dodi", "Eka"};
+        int[] nilai = {85, 92, 78, 90, 88};
+
+        System.out.println(Arrays.toString(nama));
+        System.out.println(Arrays.toString(nilai));
+    }
+}
