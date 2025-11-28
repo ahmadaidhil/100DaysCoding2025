57d5ccf510ef4def02b775387d3f9bda3655067c
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-11-28 23:16:27 +0800

    Add day082.java to print array values.

---
+++ day082.java
@@ -0,0 +1,8 @@
+public class day082 {
+    public static void main(String[] args) {
+        int[] nilai = {70, 80, 90, 85, 75, 65, 50, 15, 20};
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Nilai indeks " + i + ": " + nilai[i]);
+        }
+    }
+}
