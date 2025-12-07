b491c50564f0d99d0396e5e74a1552df38ab8974
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-12-07 20:54:24 +0800

    Parameter method.

---
+++ day091.java
@@ -0,0 +1,9 @@
+public class day091 {
+    public static void hallo(String nama, int umur) {
+        System.out.println("Hallo, " + nama);
+        System.out.println("Umur : " + umur);
+    }
+    public static void main(String[] args) {
+        hallo("Aidil", 25);
+    }
+}
