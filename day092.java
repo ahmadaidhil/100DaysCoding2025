460985bb79cdcd5b45f37c7fb1f84f6536a7086c
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-12-08 22:22:43 +0800

    Keyword return.

---
+++ day092.java
@@ -0,0 +1,9 @@
+public class day092 {
+    public static int tambah(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil = tambah(10, 20);
+        System.out.println("Hasil Penjumlahan: " + hasil);
+    }
+}
