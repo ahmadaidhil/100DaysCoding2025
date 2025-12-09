ade6d7872d347c00b7666d52aaf52e6d3184be53
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-12-09 22:15:19 +0800

    Method ceil(), floor() dan round().

---
+++ day093.java
@@ -0,0 +1,8 @@
+public class day093 {
+    public static void main(String[] args) {
+        double nilai = 7.7;
+        System.out.println("Ceil: " + Math.ceil(nilai));
+        System.out.println("Floor: " + Math.floor(nilai));
+        System.out.println("Round: " + Math.round(nilai));
+    }
+}
