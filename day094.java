5484831c36964a62f59a5b0ee9f44a7517dd3def
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-12-10 23:31:33 +0800

    Latihan: Mencetak dengan Method Void.

---
+++ day094.java
@@ -0,0 +1,9 @@
+public class day094 {
+    static void cetakPesan(String pesan) {
+        System.out.println(pesan);
+    }
+    public static void main(String[] args) {
+        cetakPesan("Halo guys");
+        cetakPesan("Ini method void.");
+    }
+}
