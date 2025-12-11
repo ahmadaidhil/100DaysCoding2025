c8219ff933ab5301a43e9ab27636931bdc2b2685
Achmad Aidhil <achmadaidhil2@gmail.com>
2025-12-11 22:58:07 +0800

    Latihan: Mencetak dengan Parameter Void.

---
+++ da095.java
@@ -0,0 +1,10 @@
+public class day095 {
+    static void cetakPesan(String pesan, int angka) {
+        System.out.println(pesan + " " + angka);
+    }
+    
+    public static void main(String[] args) {
+        cetakPesan("Halo guys", 2024);
+        cetakPesan("Ini method void dengan parameter.", 100);
+    }
+}
